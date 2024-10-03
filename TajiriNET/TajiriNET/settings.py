84051f62a193b57f73c040a2450ce8c27f5bd1ee
Moseskuriadev <moseblastajr@gmail.com>
2024-10-03 14:46:20 +0300

    commit

--- TajiriNET/TajiriNET/settings.py
+++ TajiriNET/TajiriNET/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--1ejx2_a@2u8gk8xfwon#*l%@v%ut@qc1sogs50nuuhd*#x$_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mosedev.pythonanywhere.com']
 
 
 # Application definition
