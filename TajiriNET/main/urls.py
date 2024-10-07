773e9b9c0720b9ae486ce902b47a79485f80de2d
Moseskuriadev <moseblastajr@gmail.com>
2024-10-07 14:58:05 +0300

    commit

--- TajiriNET/main/urls.py
+++ TajiriNET/main/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main-home'),  # Map the root URL of core to the index view
+    path('', views.index, name='homepage'),  # Map the root URL of core to the index view
+    path('our-services/', views.our_services, name='our-services'),
+    path('our-company/', views.our_company, name='our-company')
 ]
--- TajiriNET/main/views.py
+++ TajiriNET/main/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')  # Ensure the template path matches your structure
+
+def our_services(request):
+    return render(request, 'our-services.html')
+
+def our_company(request):
+    return render(request, 'our-company.html')
