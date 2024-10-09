89d415da92e28930bceedffb7ad6365ab9760177
Moseskuriadev <moseblastajr@gmail.com>
2024-10-09 20:37:36 +0300

    commit

--- TajiriNET/auth_purchase/admin.py
+++ TajiriNET/auth_purchase/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Plan
+from .models import Plan, MpesaPayment
 
 class PlanAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'duration')  # Columns to display in the admin list view
     search_fields = ('name',)  # Searchable fields in the admin
 
 admin.site.register(Plan, PlanAdmin)
+
+admin.site.register(MpesaPayment)
