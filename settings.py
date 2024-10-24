573566e7a0272288bd206b0943403abb3330524b
Moseskuriadev <moseblastajr@gmail.com>
2024-10-24 08:31:27 +0300

    commit3

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--1ejx2_a@2u8gk8xfwon#*l%@v%ut@qc1sogs50nuuhd*#x$_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mosedev.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['mosedev.pythonanywhere.com', '127.0.0.1', 'tajirinet.co.ke']
 
 
 # Application definition
